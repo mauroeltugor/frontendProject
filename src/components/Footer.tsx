f4d2dd4c076513237988ca36543e3ebcfac779e8
juanma1510 <jmgarca4@misena.edu.co>
2024-02-09 11:30:06 -0500

    modificacion completa footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 // Footer.js
-
+import mail from './img/correo-electronico.png';
+import phone from './img/boton-de-simbolo-de-telefono.png';
+import facebook from './img/facebook.png';
+import x from './img/gorjeo.png';
+import linkedin from './img/logotipo-de-linkedin.png';
+import ig from './img/instagram.png';
 import '../assets/Footer.css';
 
 const Footer = () => {
@@ -8,27 +13,30 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-section">
           <div className="footer-logo">ParkingLocation</div>
-          <ul className="footer-links">
-            <li><a href="/home">Home</a></li>
-            <li><a href="/ContactUs">contactanos</a></li>
-            <li><a href="/dashboard">Login</a></li>
-            <li><a href="/signup">Registro</a></li>
-          </ul>
+
         </div>
 
         <div className="footer-section">
-          <h4>Contactanos</h4>
-          <p>Email: info@yourcompany.com</p>
-          <p>Phone: +57 3148448534</p>
+          <h3 className='contactanos'>Contactanos</h3>
+          <section className="icons">
+            <div className='infop'>
+              <span className='icon'><img src={mail} alt="" className='a-icon' /></span>
+              <h4 className='info1'>support@parkinglocation.com</h4>
+            </div>
+            <div className='infop'>
+              <span className='icon'><img src={phone} alt="" className='a-icon' /></span>
+              <h4 className='info1'>+57 314 844 8534</h4>
+            </div>
+          </section>
         </div>
 
         <div className="footer-section">
-          <h4>Follow Us</h4>
+          <h3 className='contactanos'>Siguenos</h3>
           <div className="footer-social">
-            <a href="#facebook">Facebook</a>
-            <a href="#twitter">Twitter</a>
-            <a href="#linkedin">LinkedIn</a>
-            <a href="#instagram">Instagram</a>
+            <a href="#facebook"><img src={facebook} alt="" className='b-icon' /></a>
+            <a href="#twitter"><img src={x} alt="" className='b-icon' /></a>
+            <a href="#linkedin"><img src={linkedin} alt="" className='b-icon' /></a>
+            <a href="#instagram"><img src={ig} alt="" className='b-icon' /></a>
           </div>
         </div>
       </div>
