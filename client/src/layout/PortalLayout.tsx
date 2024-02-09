2d23a93f03a9c27de3ec850096e46e9cdb30ae43
Maria Luisa <117184776+marialuisaalonso850@users.noreply.github.com>
2024-02-09 10:26:57 -0500

    Update PortalLayout.tsx

--- client/src/layout/PortalLayout.tsx
+++ client/src/layout/PortalLayout.tsx
@@ -9,7 +9,7 @@ export default function PortalLayout({children}: {children:React.ReactNode}){
         e.preventDefault();
         try {
             const response = await fetch(`${API_URL}/signout`,{
-                method: "DELETE",
+                method: "GET",
                 headers:{
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${auth.getRefreshToken()}`
